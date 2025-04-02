76aad838e0d3615580c4e3790d5cbabcc644dc9e
Fips0L <f.harapinski@gmail.com>
2025-04-02 12:50:36 +0000

    Trójkąt do góry nogami

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,18 @@
-// import static org.junit.jupiter.api.Assertions.assertEquals;
-
-// import org.junit.jupiter.api.Test;
+import java.util.Scanner;
 
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    Scanner scanner = new Scanner(System.in);
+    System.out.print("Podaj wysokość trójkąta: ");
+    int wysokosc = scanner.nextInt();
+    for (int i = wysokosc; i >= 1; i--) {
+      for (int j = 1; j <= wysokosc - i; j++) {
+        System.out.print(" ");
+      }
+      for (int j = 1; j <(2*i); j++) {
+        System.out.print("*");
+      }
+      System.out.println();
+    }
   }
-
-  // @Test
-  // void addition() {
-  //     assertEquals(2, 1 + 1);
-  // }
 }
